df0eb8c06b3bd19cca7c864777b9a5441abb5c29
Jasmine <jasmine@stp.dias.ie>
2025-09-05 14:42:37 +0100

    Jinja generateVeffSubModule

--- src/Veff_generation/generate_veff_module.py
+++ src/Veff_generation/generate_veff_module.py
@@ -113,61 +113,48 @@ def generateVeffModule(filename, loopOrder, allSymbols):
      
 
 def generateVeffSubModule(name, moduleName, veffFp, allSymbols):
-    """Creates a cython module with a function that evaluates an expression for
-    Veff. 
-    
-    The expression is assumed to be broken into a list of `terms` to be added 
-    together. The sign of each term should be in `signs` and `params` is an 
-    array of parameters that appear in the expression.
-    """
+    # Creates a cython module with a function that evaluates an expression for Veff
         
     lines = read_lines(veffFp)
-    params, signs, terms = get_terms(lines)
+    signs, terms = get_terms(lines)
+    
+    processed_terms = [['+=', convert_to_cython_syntax(terms[0])]]
+    
+    for sign, term in zip(signs, terms[1:]):
+        processed_term = convert_to_cython_syntax(term)
+        sign_op = '+=' if sign > 0 else '-='
+        processed_terms.append((sign_op, processed_term))
+            
     with open(moduleName, 'w') as file:
-        # Function imports used by Veff
-        file.write('# cython: cdivision=False\n')
-        file.write('from libc.complex cimport csqrt\n')
-        file.write('from libc.complex cimport clog\n')
-        file.write('from libc.math cimport M_PI\n')
-        file.write('\n')
         
-        # Function declaration and inputs
-        file.write(f'cpdef double complex {name}(\n')
-        
-        for param in allSymbols:
-            file.write(f'    double complex {param},\n')
-        
-        file.write('    ):\n')
-        ## Calling _name maybe increases perfomance, within noise if it does
-        file.write(f'    return _{name}(\n')
-        for param in allSymbols:
-            file.write(f'        {param},\n')
-        file.write('    )\n\n\n')
-        
-        file.write(f'cdef double complex _{name}(\n')
-        
-        for param in allSymbols:
-            file.write(f'    double complex {param},\n')
-        
-        file.write('    ):\n')
-        
-        # Function body
-        file.write('    cdef double complex a = 0.0\n')
-        term = convert_to_cython_syntax(terms[0])
-        ## How do we know term[0] is +?
-        file.write(f'    a += {term}\n')
-        
-        for sign, term in zip(signs, terms[1:]):
-            term = convert_to_cython_syntax(term)
-            if sign > 0:
-                file.write(f'    a += {term}\n')
-            else:
-                file.write(f'    a -= {term}\n')
-        
-        file.write('    return a\n')
-        
-    return
-        
+        file.write(Environment().from_string(dedent("""\
+            #cython: cdivision=False
+            from libc.complex cimport csqrt
+            from libc.complex cimport clog
+            
+            cpdef double complex {{ name }}(
+            {%- for symbol in allSymbols %}
+                double complex {{ symbol }},
+            {%- endfor %}
+                ):
+                ## Calling _name decreases compile time, maybe increases perfomance
+                return _{{ name }}(
+            {%- for symbol in allSymbols %}
+                    {{ symbol }},
+            {%- endfor %}
+                )
+            
+            cdef double complex _{{ name }}(
+            {%- for symbol in allSymbols %}
+                double complex {{ symbol }},
+            {%- endfor %}
+                ):
+                cdef double complex a = 0.0
+            {%- for op, term in processed_terms %}
+                a {{ op }} {{ term }}
+            {%- endfor %}
+                return a
+            """)).render(name=name, allSymbols=allSymbols, processed_terms=processed_terms))
 
 
 def convert_to_cython_syntax(term):
