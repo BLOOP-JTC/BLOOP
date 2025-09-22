32466816625419cdb04b9a995a5671884f54b464
Jasmine <jasmine@stp.dias.ie>
2025-09-22 18:08:16 +0100

    Factor signs and expression handling

--- src/Veff_generation/generate_veff_module.py
+++ src/Veff_generation/generate_veff_module.py
@@ -113,8 +113,6 @@ def generateVeffModule(filename, loopOrder, allSymbols):
      
 def generateVeffSubModule(name, moduleName, veffFp, allSymbols):
     # Creates a cython module with that computes an order of Veff
-        
-    opsAndTerms = np.transpose(read_and_parse_terms(veffFp))
     with open(moduleName, 'w') as file:
     
         file.write(Environment().from_string(dedent("""\
@@ -140,13 +138,47 @@ def generateVeffSubModule(name, moduleName, veffFp, allSymbols):
             {%- endfor %}
                 ):
                 cdef double complex a = 0.0
-            {%- for op, term in opsAndTerms %}
+            {%- for op, term in opsAndExpressions %}
                 a {{ op }} {{ term }}
             {%- endfor %}
                 return a
-            """)).render(name=name, allSymbols=allSymbols, opsAndTerms=opsAndTerms))
+            """)).render(name=name, allSymbols=allSymbols, opsAndExpressions=np.transpose(mutliLineExpression(veffFp))))
 
 
+
+def mutliLineExpression(filePointer):
+    ## Takes an expressions and breaks it down into a mutli line expression
+    ## (Cython seems to struggle with the one line NNLO veff)
+    
+    with open(filePointer, 'r') as file:
+        veff = file.read()
+    
+    operations = ["+="]
+    expressions = []
+    
+    netBrackets = 0
+    start = 0
+    
+    for i, char in enumerate(veff):
+        if char == '(':
+            netBrackets += 1
+        elif char == ')':
+            netBrackets -= 1
+        if char == ' ' and netBrackets == 0:
+            ##+1 to catch space
+            line = veff[start:i+1]
+            if line in ["+ ", "- "]:
+                operations.append("+=" if line == "+ " else "-=")
+            else:
+                expressions.append(convert_to_cython_syntax(line))
+            start = i + 1
+    
+    # Any remaining characters should just be expressions
+    if start < len(veff):
+        line = veff[start:]
+        expressions.append(convert_to_cython_syntax(line))
+    return operations, expressions
+    
 def convert_to_cython_syntax(term):
     term = term.replace('Sqrt', 'csqrt')
     term = term.replace('Log', 'clog')
@@ -155,37 +187,3 @@ def convert_to_cython_syntax(term):
     term = term.replace('^', '**')
     term = PythoniseMathematica.replaceSymbolsConst(term)
     return PythoniseMathematica.replaceGreekSymbols(term)
-
-
-def mutliLineExpression(filename):
-    ## Takes an expressions and breaks it down into a mutli line expression
-    ## (Cython seems to struggle with the one line NNLO veff)
-    num_unpaired_brackets = 0
-    next_line = []
-    terms = []
-    operations = ["+="]
-    
-    with open(filename, 'r') as file:
-        while True:
-            char = file.read(1)
-            if not char:
-                terms.append(convert_to_cython_syntax(''.join(next_line)))
-                break
-
-            next_line.append(char)
-
-            if char == '(':
-                num_unpaired_brackets += 1
-            elif char == ')':
-                num_unpaired_brackets -= 1
-
-            if char == ' ' and num_unpaired_brackets == 0:
-                line = ''.join(next_line)
-                if line in ["+ ", "- "]:
-                    operations.append("+=" if line == "+ " else "-=")
-                else:
-                    terms.append(convert_to_cython_syntax(line))
-    
-                next_line = []
-    
-    return operations, terms
